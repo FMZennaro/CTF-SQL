c8c4feb6ac74e27d9e6768ffa6e593113b4a1c37
avalds <avalds@gmail.com>
2021-02-23 16:44:21 +0100

    some more results

--- evaluate17.py
+++ evaluate17.py
@@ -52,9 +52,42 @@ def evaluate_random(env, num_steps=1000):
 
 	mean_reward = round(np.mean(episode_rewards), 3)
 	return mean_reward, len(episode_rewards)-1
+def test_episodes(model, env, num_episodes, max_steps = 1000):
+	episode_rewards = []
+	for j in range(num_episodes):
+		done = False
+		steps = 0
+		obs = env.reset()
+		episode_rewards.append(0.0)
+		while(not done and steps < max_steps):
+			action, _states = model.predict(obs, deterministic = True)
+			obs, reward, done, _ = env.step(action)
+
+			episode_rewards[-1] += reward
+			steps += 1
+			#print("steps", steps, end = ",")
+		print("j", j, end = " ")
+
+
+	return(episode_rewards)
+def plot_evaluation_determistic(model, env, num_steps):
+	episode_rewards = [0.0]
+	obs = env.reset()
+	for i in range(num_steps):
+		action, _states = model.predict(obs, deterministic = True)
+		obs, reward, done, _ = env.step(action)
+
+		episode_rewards[-1] += reward
+		if done:
+			obs = env.reset()
+			episode_rewards.append(0.0)
+
+	mean_reward = round(np.mean(episode_rewards), 3)
+	median_reward = np.median(episode_rewards)
+
+	return(episode_rewards)
 
 def plot_evaluation(model, env, num_steps):
-	import matplotlib.pyplot as plt
 	episode_rewards = [0.0]
 	obs = env.reset()
 	for i in range(num_steps):
@@ -71,5 +104,4 @@ def plot_evaluation(model, env, num_steps):
 			obs = env.reset()
 			episode_rewards.append(0.0)
 
-	print(episode_rewards)
-	plt.plot(episode_rewards)
+	return(episode_rewards)
